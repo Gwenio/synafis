435959bfd2a189cc31b871542e9de5bbc85a081b
James Adam Armstrong <gwenio@live.com>
2018-12-22 13:44:15 -0600

    Added initial stubs for unit testing gc::allocator.

---
+++ test/gc/allocator.hpp
@@ -0,0 +1,60 @@
+
+/*
+ISC License (ISC)
+
+Copyright 2018 Adam Armstrong
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+*/
+
+/**	\file test/gc/allocator.cpp
+ *	\brief Defines test cases for gc::allocator.
+ */
+
+#include "../../src/gc/allocator.hpp"
+#include "../master.hpp"
+
+namespace unit_test {
+
+/**	\class tester<gc::allocator>
+ *	\brief Class with access to the interals of gc::allocator to make tests.
+ */
+template<>
+class tester<gc::allocator> {
+	/**	\fn tester()
+	 *	\brief Deleted.
+	 */
+	tester() = delete;
+
+	/**	\fn ~tester()
+	 *	\brief Deleted.
+	 */
+	~tester() = delete;
+public:
+	/**	\typedef allocator
+	 *	\brief Shorthand for the class being tested.
+	 */
+	typedef gc::allocator allocator;
+
+	/**	\typedef handle
+	 *	\brief Shorthand for a pool::handle.
+	 */
+	typedef pool::handle handle;
+
+	/**	\fn creation(collector &)
+	 *	\brief Tests the construction of an allocator.
+	 */
+	static void creation(collector &);
+};
+
+}
