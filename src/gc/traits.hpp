f2ad6579b8524dc635c819ed3a336cc5195dcc27
James Adam Armstrong <gwenio@live.com>
2018-12-25 14:47:56 -0600

    Fixed up documentation in src/gc/traits.hpp to make Doxygen happy.

--- src/gc/traits.hpp
+++ src/gc/traits.hpp
@@ -47,7 +47,7 @@ typedef std::bitset<5> flag_type;
  *	\details All trivial copyable types are considered movable by default.
  *	\details
  *	\details Other types should have a specialization defined.
- *	\see movable<T>
+ *	\see movable\<T\>
  */
 template<typename T>
 class moveable_type : public std::is_trivially_copyable<T> {};
@@ -55,15 +55,15 @@ class moveable_type : public std::is_trivially_copyable<T> {};
 /**	\var movable
  *	\brief A boolean value indicating if objects of a type can be moved by the collector.
  *	\tparam T The type the value is for. Will be stripped of const and volatile qualifiers.
- *	\details If true while relocator<T> == nullptr, then it means objects of
+ *	\details If true while relocator\<T\> == nullptr, then it means objects of
  *	\details the type may be copied via simply copying the contents of memory.
  *	\details
- *	\details If true while relocator<T> != nullptr, then it means objects of
- *	\details the type will be relocated by relocator<T>.
+ *	\details If true while relocator\<T\> != nullptr, then it means objects of
+ *	\details the type will be relocated by relocator\<T\>.
  *	\details
  *	\details If false then objects of the type cannot be moved by the collector.
- *	\note To make a type movable, specialize movable_type<T>.
- *	\see movable_type<T>
+ *	\note To make a type movable, specialize movable_type\<T\>.
+ *	\see movable_type\<T\>
  */
 template<typename T>
 inline constexpr bool const movable{moveable_type<std::remove_cv_t<T>>::value};
@@ -77,7 +77,7 @@ inline constexpr bool const movable{moveable_type<std::remove_cv_t<T>>::value};
  *	\details cannot be mutated even in the absence of a const qualifier.
  *	\details
  *	\details Other types should have a specialization defined.
- *	\see readonly<T>
+ *	\see readonly\<T\>
  */
 template<typename T>
 class readonly_type : public std::is_const<T> {};
@@ -88,12 +88,12 @@ class readonly_type : public std::is_const<T> {};
  *	\details When types are labelled as being immutable, certain garbage collector
  *	\details optimizations become possible.
  *	\details
- *	\details When true equalizer<T> must not be nullptr.
+ *	\details When true equalizer\<T\> must not be nullptr.
  *	\details
  *	\details The modification of pointers due to remapping is not considered
  *	\details mutation for the purposes of this variable.
- *	\note To make a type readonly, specialize readonly_type<T>.
- *	\see readonly_type<T>
+ *	\note To make a type readonly, specialize readonly_type\<T\>.
+ *	\see readonly_type\<T\>
  */
 template<typename T>
 inline constexpr bool const readonly{readonly_type<std::remove_volatile_t<T>>::value};
@@ -104,7 +104,7 @@ inline constexpr bool const readonly{readonly_type<std::remove_volatile_t<T>>::v
  *	\details All types are listed as not holding pointers by default.
  *	\details
  *	\details Other types should have a specialization defined.
- *	\see pointers<T>
+ *	\see pointers\<T\>
  */
 template<typename T>
 class pointers_type : public std::integral_constant<bool, false> {};
@@ -113,8 +113,8 @@ class pointers_type : public std::integral_constant<bool, false> {};
  *	\brief A boolean value indicating if objects of a type contain pointers to other objects.
  *	\tparam T The type the value is for. Will be stripped of const and volatile qualifiers.
  *	\details When true, neither traverser nor relocator may be nullptr.
- *	\note To mark a type as containing pointers, specialize pointers_type<T>.
- *	\see pointers_type<T>
+ *	\note To mark a type as containing pointers, specialize pointers_type\<T\>.
+ *	\see pointers_type\<T\>
  */
 template<typename T>
 inline constexpr bool const pointers{pointers_type<std::remove_cv_t<T>>::value};
@@ -127,7 +127,7 @@ inline constexpr bool const pointers{pointers_type<std::remove_cv_t<T>>::value};
  *	\details
  *	\details Other types will have it set to a lambda expression that calls the destructor.
  *	\note Only specialize for types that preform clean up outside of the destructor.
- *	\see finalizer<T>
+ *	\see finalizer\<T\>
  */
 template<typename T, typename Enable = void>
 class finalize_type;
@@ -135,6 +135,9 @@ class finalize_type;
 template<typename T>
 class finalize_type<T, std::enable_if_t<std::is_trivially_destructible_v<T>>> {
 public:
+	/**	\var value
+	 *	\brief There is no finalize callback needed.
+	 */
 	static constexpr finalize_cb const value{nullptr};
 };
 
@@ -142,6 +145,9 @@ template<typename T>
 class finalize_type<T, std::enable_if_t<(std::is_destructible_v<T> &&
 	!std::is_trivially_destructible_v<T>)>> {
 public:
+	/**	\var value
+	 *	\brief The default finalize callback that calls the type's destructor.
+	 */
 	static constexpr finalize_cb const value{
 		[](void *obj) noexcept -> void {
 			SYNAFIS_ASSERT(obj != nullptr);
@@ -153,7 +159,7 @@ public:
 /**	\var finalizer
  *	\brief A pointer to the finalize_cb for a type.
  *	\tparam T The type the value is for. Will be stripped of const and volatile qualifiers.
- *	\see finalize_type<T>
+ *	\see finalize_type\<T\>
  */
 template<typename T>
 inline constexpr finalize_cb const finalizer{finalize_type<std::remove_cv_t<T>>::value};
@@ -166,8 +172,8 @@ inline constexpr finalize_cb const finalizer{finalize_type<std::remove_cv_t<T>>:
  *	\details
  *	\details For other types, call the member function traverse(void *, enumerate_cb).
  *	\note Specialize for types lacking a traverse member.
- *	\see traverser<T>
- *	\see pointers<T>
+ *	\see traverser\<T\>
+ *	\see pointers\<T\>
  */
 template<typename T, typename Enable = void>
 class traverse_type;
@@ -175,12 +181,18 @@ class traverse_type;
 template<typename T>
 class traverse_type<T, std::enable_if_t<(!pointers<T>)>> {
 public:
+	/**	\var value
+	 *	\brief There is no traversal callback needed.
+	 */
 	static constexpr traverse_cb const value{nullptr};
 };
 
 template<typename T>
 class traverse_type<T, std::enable_if_t<(pointers<T>)>> {
 public:
+	/**	\var value
+	 *	\brief The default traversal callback that calls the type's member traverse.
+	 */
 	static constexpr traverse_cb const value{
 		[](void const* obj, void *data, enumerate_cb cb) noexcept -> void {
 			SYNAFIS_ASSERT(obj != nullptr);
@@ -195,7 +207,7 @@ public:
  *	\tparam T The type the value is for. Will be stripped of const and volatile qualifiers.
  *	\details Types that do not contain pointers owned by the collector do not need
  *	\details this callback.
- *	\see traverser_type<T>
+ *	\see traverser_type\<T\>
  */
 template<typename T>
 inline constexpr traverse_cb const traverser{traverse_type<std::remove_cv_t<T>>::value};
@@ -206,14 +218,14 @@ inline constexpr traverse_cb const traverser{traverse_type<std::remove_cv_t<T>>:
  *	\tparam Enable Used to enable specializations with std::enable_if_t.
  *	\param orig The original location.
  *	\param dest The destination location.
- *	\details Used in defining relocate_type<T>.
+ *	\details Used in defining relocate_type\<T\>.
  *	\details
  *	\details If the type is nothrow move assignable, will use move assignment.
  *	\details
  *	\details If the type is nothrow copy assignable, will use copy assignment.
  *	\details
  *	\details For other cases, a specialization must be defined.
- *	\see relocator_type<T>
+ *	\see relocator_type\<T\>
  */
 template<typename T>
 void transfer(T &&orig, T &dest) noexcept;
@@ -235,16 +247,16 @@ transfer(T &&orig, T &dest) noexcept {
  *	\brief Trait for getting the relocate_cb for a type.
  *	\tparam T The type to set the callback for.
  *	\tparam Enable Used to enable specializations with std::enable_if_t.
- *	\details If pointers<T>, the defined callback will call the member function remap(void *, remap_cb).
+ *	\details If pointers\<T\>, the defined callback will call the member function remap(void *, remap_cb).
  *	\details
- *	\details For those that are movable<T> and need a relocate_cb, the callback calls
+ *	\details For those that are movable\<T\> and need a relocate_cb, the callback calls
  *	\details transfer(T &&, T &). The transfer occurs before the remap if both are preformed.
  *	\details
- *	\details For types !pointers<T> and either trivially copyable or !movable<T> the
+ *	\details For types !pointers\<T\> and either trivially copyable or !movable\<T\> the
  *	\details callback is set to nullptr.
  *	\note Specialize for types lacking a remap member.
- *	\see relocator<T>
- *	\see pointers<T>
+ *	\see relocator\<T\>
+ *	\see pointers\<T\>
  *	\see transfer(T &&, T &) noexcept
  */
 template<typename T, typename Enable = void>
@@ -254,6 +266,9 @@ template<typename T>
 class relocate_type<T, std::enable_if_t<(!pointers<T> &&
 	(std::is_trivially_copyable_v<T> || !movable<T>))>> {
 public:
+	/**	\var value
+	 *	\brief There is no relocation callback available.
+	 */
 	static constexpr relocate_cb const value{nullptr};
 };
 
@@ -261,6 +276,9 @@ template<typename T>
 class relocate_type<T, std::enable_if_t<(!pointers<T> &&
 	!std::is_trivially_copyable_v<T> && movable<T>)>> {
 public:
+	/**	\var value
+	 *	\brief The default relocate callback that calls the transfer\<T\>.
+	 */
 	static constexpr relocate_cb const value{
 		[](void *orig, void *dest, void *, remap_cb) noexcept -> void {
 			SYNAFIS_ASSERT(orig != nullptr);
@@ -275,6 +293,10 @@ public:
 template<typename T>
 class relocate_type<T, std::enable_if_t<(pointers<T> && !movable<T>)>> {
 public:
+	/**	\var value
+	 *	\brief The default traversal callback that calls the type's member remap.
+	 *	\details This version does not move the object.
+	 */
 	static constexpr relocate_cb const value{
 		[](void *orig, void *dest, void *data, enumerate_cb cb) noexcept -> void {
 			SYNAFIS_ASSERT(dest != nullptr);
@@ -288,6 +310,11 @@ public:
 template<typename T>
 class relocate_type<T, std::enable_if_t<(pointers<T> && movable<T>)>> {
 public:
+	/**	\var value
+	 *	\brief The default traversal callback that calls the type's member remap.
+	 *	\details This version moves the object to a new address with move assignment
+	 *	\details if the collector provides a new address.
+	 */
 	static constexpr relocate_cb const value{
 		[](void *orig, void *dest, void *data, remap_cb cb) noexcept -> void {
 			SYNAFIS_ASSERT(orig != nullptr);
@@ -307,7 +334,7 @@ public:
  *	\tparam T The type the value is for. Will be stripped of const and volatile qualifiers.
  *	\details For types that needs a specialized move operation or contain
  *	\details pointers to remap.
- *	\see relocator_type<T>
+ *	\see relocator_type\<T\>
  */
 template<typename T>
 inline constexpr relocate_cb const relocator{relocate_type<std::remove_cv_t<T>>::value};
@@ -317,10 +344,10 @@ inline constexpr relocate_cb const relocator{relocate_type<std::remove_cv_t<T>>:
  *	\tparam T The type being compared.
  *	\param lhs The left hand side of the comparison.
  *	\param rhs The right hand side of the comparison.
- *	\details Used in defining equality_type<T>.
+ *	\details Used in defining equality_type\<T\>.
  *	\details
  *	\details Defaults to lhs == rhs.
- *	\see equality_type<T>
+ *	\see equality_type\<T\>
  */
 template<typename T>
 bool compare(T const& lhs, T const& rhs) noexcept {
@@ -331,16 +358,16 @@ bool compare(T const& lhs, T const& rhs) noexcept {
  *	\brief Trait for getting the equality_cb for a type.
  *	\tparam Enable Used to enable specializations with std::enable_if_t.
  *	\tparam T The type to set the callback for.
- *	\details If pointers<T>, the defined callback will call the member function remap(void *, remap_cb).
+ *	\details If pointers\<T\>, the defined callback will call the member function remap(void *, remap_cb).
  *	\details
- *	\details For those that are movable<T> and need a relocate_cb, the callback calls
+ *	\details For those that are movable\<T\> and need a relocate_cb, the callback calls
  *	\details transfer(T &&, T &). The transfer occurs before the remap if both are preformed.
  *	\details
- *	\details For types !pointers<T> and either trivially copyable or !movable<T> the
+ *	\details For types !pointers\<T\> and either trivially copyable or !movable\<T\> the
  *	\details callback is set to nullptr.
- *	\note Specialize compare() and readonly<T> instead.
- *	\see equalizer<T>
- *	\see readonly<T>
+ *	\note Specialize compare() and readonly\<T\> instead.
+ *	\see equalizer\<T\>
+ *	\see readonly\<T\>
  *	\see compare(T const&, T const&) noexcept
  */
 template<typename T, typename Enable = void>
@@ -349,12 +376,18 @@ class equality_type;
 template<typename T>
 class equality_type<T, std::enable_if_t<(!readonly<T>)>> {
 public:
+	/**	\var value
+	 *	\brief There is no equality callback available.
+	 */
 	static constexpr equality_cb const value{nullptr};
 };
 
 template<typename T>
 class equality_type<T, std::enable_if_t<(readonly<T>)>> {
 public:
+	/**	\var value
+	 *	\brief The default equality callback that calls the compare\<T\>.
+	 */
 	static constexpr equality_cb const value{
 		[](void const* lhs, void const* rhs) noexcept -> bool {
 			SYNAFIS_ASSERT(lhs != rhs);
@@ -377,7 +410,7 @@ public:
  *	\details objects may be released.
  *	\note To be practical an additional callback to get a hash for immutable objects
  *	\note to quickly reject some possibilities.
- *	\see equality_type<T>
+ *	\see equality_type\<T\>
  */
 template<typename T>
 inline constexpr equality_cb const equalizer{equality_type<std::remove_volatile_t<T>>::value};
@@ -386,11 +419,11 @@ inline constexpr equality_cb const equalizer{equality_type<std::remove_volatile_
  *	\brief Gets a set of flags summarizing trait information to pass to the collector.
  *	\tparam T The type to get a summary for.
  *	\details The flags from least to most significant are:
- *	\details - movable<T>
- *	\details - finalizer<T> != nullptr
- *	\details - pointers<T>
- *	\details - relocator<T> != nullptr
- *	\details - readonly<T>
+ *	\details - movable\<T\>
+ *	\details - finalizer\<T\> != nullptr
+ *	\details - pointers\<T\>
+ *	\details - relocator\<T\> != nullptr
+ *	\details - readonly\<T\>
  *	\see flag_type
  */
 template<typename T>
