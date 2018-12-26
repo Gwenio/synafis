4cec1ccd8bfead047bd7e19ae4d709111c6eee1d
James Adam Armstrong <gwenio@live.com>
2018-12-26 11:50:35 -0600

    Corrected use of std::enable_if_t for class template specialization.
    Documentation corrections were made.

--- src/operations/generate.hpp
+++ src/operations/generate.hpp
@@ -55,7 +55,7 @@ public:
 	using type = typename std::tuple<T1, T2>;
 };
 
-/**	\class concat_type<T1, std::tuple<T2...>>
+/**
  *	\brief Makes the tuple of a type and the types from a tuple.
  *	\tparam T1 The type to concatenate.
  *	\tparam T2 The tuple to concatenate.
@@ -78,7 +78,7 @@ public:
 	using type = typename std::tuple<T1, T2...>;
 };
 
-/**	\class concat_type<std::tuple<T1...>, T2>
+/**
  *	\brief Makes the tuple of a type and the types from a tuple.
  *	\tparam T1 The tuple to concatenate.
  *	\tparam T2 The type to concatenate.
@@ -101,7 +101,7 @@ public:
 	using type = typename std::tuple<T1..., T2>;
 };
 
-/**	\class concat_type<std::tuple<T1...>, std::tuple<T2...>>
+/**
  *	\brief Makes the tuple by concatenating two other tuples' types.
  *	\tparam T1 The first tuple to concatenate.
  *	\tparam T2 The second tuple to concatenate.
@@ -158,33 +158,36 @@ using append_if = typename std::conditional_t<Cond, concat<T1, T2>, T1>;
 /**	\class flatten_type
  *	\brief Flattens a tuple so the contents of contained tuples are present in the result.
  *	\tparam T The tuple type to flatten.
+ *	\tparam Enable Used to enable specializations with std::enable_if_t.
  */
-template<typename T>
+template<typename T, typename Enable = void>
 class flatten_type;
 
-/**	\class flatten_type<std::enable_if_t<(2 <= sizeof...(Tail)), std::tuple<Head, Tail...>>>
+/**
  *	\brief Flattens a tuple so the contents of contained tuples are present in the result.
  *	\tparam Head The first element to flatten.
  *	\tparam Tail The remaining elements to flatten.
+ *	\see flatten_type\<T, Enable\>
  */
 template<typename Head, typename... Tail>
-class flatten_type<std::enable_if_t<(2 <= sizeof...(Tail)), std::tuple<Head, Tail...>>> :
+class flatten_type<std::tuple<Head, Tail...>, std::enable_if_t<(2 <= sizeof...(Tail))>> :
 	public concat_type<Head, flatten_type<std::tuple<Tail...>>::type> {};
 
-/**	\class flatten_type
+/**
  *	\brief Flattens two tuple elements to form a single tuple.
  *	\tparam Head The first element to flatten.
  *	\tparam Tail The remaining element to flatten.
+ *	\see flatten_type\<T, Enable\>
  */
 template<typename Head, typename Tail>
 class flatten_type<std::tuple<Head, Tail>> : public concat_type<Head, Tail> {};
 
 /**	\typedef flatten
- *	\brief Shorthand for typename flatten_type<std::tuple<Types...>>::type.
+ *	\brief Shorthand for typename flatten_type\<std::tuple\<Types...\>\>::type.
  *	\tparam Types The types to flatten into a single tuple.
- *	\note If there is only two types then it is equivalent to concat<Types...>.
+ *	\note If there is only two types then it is equivalent to concat\<Types...\>.
  *	\note As such using concat directly in such cases may reduce compile time.
- *	\see flatten_type
+ *	\see flatten_type\<T, Enable\>
  */
 template<typename... Types>
 using flatten = typename flatten_type<std::tuple<Types...>>::type;
