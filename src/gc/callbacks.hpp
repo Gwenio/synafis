9068e4804b5dab3e4eeb4a778f9208265ff69cfa
James Adam Armstrong <gwenio@live.com>
2019-01-15 19:27:47 -0600

    Fixed copyright dates.

--- src/gc/callbacks.hpp
+++ src/gc/callbacks.hpp
@@ -2,7 +2,7 @@
 /*
 ISC License (ISC)
 
-Copyright 2018 Adam Armstrong
+Copyright 2018-2019 Adam Armstrong
 
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted, provided that the above copyright
