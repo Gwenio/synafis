72b8a68a69888405a12cee3bf18f6335401908b6
James Adam Armstrong <gwenio@live.com>
2019-02-09 12:25:21 -0600

    Updated 'js/ninja.js' to place Lodash imports into the '_' variable.
    Also updated the exports so the types for Writer and Build can be imported by TypeScript without actually importing the file.

--- js/ninja.js
+++ js/ninja.js
@@ -23,55 +23,59 @@
 "use strict";
 
 // spellcheck: off
-const merge = require('lodash/merge')
-const map = require('lodash/map')
-const reduce = require('lodash/reduce')
-const find = require('lodash/find')
-const reject = require('lodash/reject')
-const some = require('lodash/some')
-const concat = require('lodash/concat')
-const partition = require('lodash/partition')
-const set = require('lodash/set')
-const get = require('lodash/get')
-const each = require('lodash/forEach')
-const size = require('lodash/size')
-const pick = require('lodash/pick')
-const isNil = require('lodash/isNil')
-const join = require('lodash/join')
+const _ = {}
+_.merge = require('lodash/merge')
+_.map = require('lodash/map')
+_.reduce = require('lodash/reduce')
+_.find = require('lodash/find')
+_.reject = require('lodash/reject')
+_.some = require('lodash/some')
+_.concat = require('lodash/concat')
+_.partition = require('lodash/partition')
+_.set = require('lodash/set')
+_.get = require('lodash/get')
+_.each = require('lodash/forEach')
+_.size = require('lodash/size')
+_.pick = require('lodash/pick')
+_.isNil = require('lodash/isNil')
+_.join = require('lodash/join')
 const { promises: fs } = require('fs')
 const path = require('path')
 // spellcheck: on
 
+/** @returns {{ [id:string]: number }} */
 function process_pools(raw)
 {
-	return reduce(raw, (acc, cur, key) =>
-		set(acc, key, Math.round(get(cur, 'depth', 1))), {})
+	return _.reduce(raw, (acc, cur, key) =>
+		_.set(acc, key, Math.round(_.get(cur, 'depth', 1))), {})
 }
 
+/** @returns {{ [id:string]: string }} */
 function process_def_variables(raw)
 {
-	return reduce(raw, (acc, cur, id) =>
-		set(acc, id, get(cur, 'default', '')), {})
+	return _.reduce(raw, (acc, cur, id) =>
+		_.set(acc, id, _.get(cur, 'default', '')), {})
 }
 
+/** @returns {{ [id:string]: object[] }} */
 function process_variables(raw)
 {
-	return reduce(raw, (acc, cur, id) =>
-		set(acc, id, get(cur, 'specific', [])), {})
+	return _.reduce(raw, (acc, cur, id) =>
+		_.set(acc, id, _.get(cur, 'specific', [])), {})
 }
 
 function process_rules(raw)
 {
-	return reduce(raw, (acc, val, key) =>
-		set(acc, key, get(val, 'vars', '')), {})
+	return _.reduce(raw, (acc, val, key) =>
+		_.set(acc, key, _.get(val, 'vars', '')), {})
 }
 
 function process_actions(raw)
 {
-	return map(raw, (val, key) =>
+	return _.map(raw, (val, key) =>
 	{
 		return {
-			filters: get(val, 'filters', []),
+			filters: _.get(val, 'filters', []),
 			action: val.action,
 			id: key
 		}
@@ -94,7 +98,7 @@ class Writer
 	{
 		this.order = this.order.finally(async () =>
 		{
-			if (!isNil(this.file))
+			if (!_.isNil(this.file))
 			{
 				let temp = this.file
 				this.file = null
@@ -124,9 +128,9 @@ class Writer
 	 */
 	variable(name, value, indent)
 	{
-		if (!(isNil(value) || value === ''))
+		if (!(_.isNil(value) || value === ''))
 		{
-			this.write(`${indent ? '  ' : ''} ${name} = ${value}\n`)
+			this.write(`${indent ? '  ' : ''}${name} = ${value}\n`)
 		}
 		return this
 	}
@@ -152,59 +156,63 @@ class Writer
 	rule(variables, key)
 	{
 		this.write(`rule ${key}\n`)
-		each(variables, (x, y) => this.variable(y, x, true))
+		_.each(variables, (x, y) => this.variable(y, x, true))
 		return this
 	}
 
 	/**
-	 *
+	 * @private
 	 * @param {string} pre
-	 * @param {Array<string>} items
+	 * @param {string[]} items
 	 * @returns {Writer} this
 	 */
 	buildList(pre, items)
 	{
-		if (size(items) > 0)
+		if (items !== [])
 		{
 			this.write(pre)
-			each(items, (val) => this.write(` ${val}`))
+			_.each(items, (val) => this.write(` ${val}`))
 		}
 		return this
 	}
 
 	/**
 	 * Outputs a build entry to the Ninja file.
-	 * @param {Build} param0
+	 * @param {Build} build
 	 * @returns {Writer} this
 	 */
 	build({ action, outputs, implicit, inputs, depends, after, vars })
 	{
 		this.write("build")
-		each(outputs, (val) => this.write(` ${val}`))
+		_.each(outputs, (val) => this.write(` ${val}`))
 		this.buildList(" |", implicit)
 			.write(`: ${action}`)
-		each(inputs, (val) => this.write(` ${val}`))
+		_.each(inputs, (val) => this.write(` ${val}`))
 		this.buildList(" |", depends)
 			.buildList(" ||", after)
 			.write("\n")
-		each(vars, (val, key) => this.variable(key, val, true))
+		_.each(vars, (val, key) => this.variable(key, val, true))
 		return this
 	}
 
 	/**
 	 * Outputs a default line to the Ninja file.
-	 * @param {Array<string>} targets The build targets to be defauls.
+	 * @param {string[]} targets The build targets to be defauls.
 	 * @returns {Writer} this
 	 */
 	defs(targets)
 	{
-		if (size(targets) > 0)
+		if (targets !== [])
 		{
-			this.write(`default ${join(targets, ' ')}\n`)
+			this.write(`default ${_.join(targets, ' ')}\n`)
 		}
 		return this
 	}
 }
+
+/**
+ * @typedef {{path: string, origin: string}} ProductPath
+ */
 
 /**
  * @type Build
@@ -213,73 +221,84 @@ class Build
 {
 	/**
 	 *
-	 * @param {String} action
-	 * @param {*} outputs
-	 * @param {*} implicit
-	 * @param {*} inputs
-	 * @param {*} depends
-	 * @param {*} after
-	 * @param {*} vars
+	 * @param {string} action
+	 * @param {{path: string}[]} outputs
+	 * @param {{path: string}[]} implicit
+	 * @param {{path: string}[]} inputs
+	 * @param {{path: string}[]} depends
+	 * @param {{path: string}[]} after
+	 * @param {{ [name: string]: string }} vars
 	 */
 	constructor(action, outputs, implicit, inputs, depends, after, vars)
 	{
 		this.action = action
-		this.outputs = map(outputs, 'path')
-		this.implicit = map(implicit, 'path')
-		this.inputs = map(inputs, 'path')
-		this.depends = map(depends, 'path')
-		this.after = map(after, 'path')
+		this.outputs = _.map(outputs, 'path')
+		this.implicit = _.map(implicit, 'path')
+		this.inputs = _.map(inputs, 'path')
+		this.depends = _.map(depends, 'path')
+		this.after = _.map(after, 'path')
 		this.vars = vars
 	}
 
 	/**
 	 * Maps build steps to Ninja rules based on their action.
-	 * @param {Object} acts
-	 * @param {Object} steps
-	 * @returns {Object}
+	 * @typedef {{ [id:string]: string }} ActionMap
+	 * @param {ActionMap} acts
+	 * @param {{ [id:string]: { action: string }}} steps
+	 * @returns {ActionMap}
 	 */
 	static process_actions(acts, steps)
 	{
 		/**
-		 * @param {Object} acc
+		 * @param {ActionMap} acc
 		 * @param {Object} step
 		 * @param {string} step.action
 		 * @param {string} id
-		 * @returns {Object} acc
+		 * @returns {ActionMap} acc
 		 */
 		function reduction(acc, { action }, id)
 		{
-			return set(acc, id, acts[action])
+			return _.set(acc, id, acts[action])
 		}
-		return reduce(steps, reduction, {})
+		return _.reduce(steps, reduction, {})
 	}
 
-	static process_sources(root, sources, steps)
+	/**
+	 *
+	 * @param {string} root
+	 * @param {*} steps
+	 * @param {*} sources
+	 */
+	static process_sources(root, steps, sources)
 	{
-		return reduce(steps, (acc, item, id) =>
-			set(acc, id, map(item.sources, (src) =>
+		return _.reduce(steps, (acc, item, id) =>
+			_.set(acc, id, _.map(item.sources, (src) =>
 			{
-				const files = map(map(get(sources, src.group, []),
+				const files = _.map(_.map(_.get(sources, src.group, []),
 						(file) => path.parse(path.join(root, file))), (file) =>
-					set(file, 'origin', path.join(file.dir, file.name)))
-				return merge({ files: files },
-					pick(src, ['implicit', 'produces']))
+					_.set(file, 'origin', path.join(file.dir, file.name)))
+				return _.merge({ files: files },
+					_.pick(src, ['implicit', 'produces']))
 			})), {})
 	}
 
+	/**
+	 * @param {any} names
+	 * @param {any} steps
+	 */
 	static process_products(names, steps)
 	{
-		return reduce(steps, (outer, outputs, key) =>
+		return _.reduce(steps, (outer, outputs, key) =>
 		{
-			const temp = reduce(outputs,
+			const temp = _.reduce(outputs,
 				(inner, { implicit, products, location, extension }, id) =>
-				(isNil(products) || size(products) < 1) ? inner :
-				set(inner, id,
+				(_.isNil(products) || products === []) ? inner :
+				_.set(inner, id,
 				{
 					implicit,
-					files: map(products, (name) =>
+					files: _.map(products, (name) =>
 					{
-						const origin = get(names, name, name)
+						const origin = _.get(names, name, name)
 						return {
 							dir: location,
 							name: origin,
@@ -288,55 +307,56 @@ class Build
 						}
 					})
 				}), {})
-			return size(temp) < 1 ? outer : set(outer, key, temp)
+			return _.size(temp) < 1 ? outer : _.set(outer, key, temp)
 		}, {})
 	}
 
 	/**
 	 *
 	 * @param {String} name
-	 * @param {*} inputs
+	 * @param {ProductPath[]} inputs
 	 * @returns {Build}
 	 */
 	static alias(name, inputs)
 	{
-		return new Build('phony', [{ path: name }],
-			[], inputs, [], [], {})
+		return new Build('phony', [{ path: name }], [], inputs, [], [], {})
 	}
 
 	/**
 	 *
 	 * @param {String} action
-	 * @param {Object} param1
-	 * @param {*} param1.single
-	 * @param {*} param1.multiple
-	 * @param {*} implicit
-	 * @param {*} inputs
-	 * @param {*} depends
-	 * @param {*} after
-	 * @param {*} vars
-	 * @returns {Array<Build>}
+	 * @param {{single: ProductPath[], multiple: ProductPath[]}} outputs
+	 * @param {ProductPath[]} implicit
+	 * @param {ProductPath[]} inputs
+	 * @param {ProductPath[]} depends
+	 * @param {ProductPath[]} after
+	 * @param {{ [name: string]: string }} vars
+	 * @returns {Build[]}
 	 */
 	static create(action, { single, multiple }, implicit, inputs, depends, after, vars = {})
 	{
-		if (size(single) < 1)
+		if (single === [])
 		{
 			return [new Build(action, multiple, implicit, inputs, depends, after, vars)]
 		}
 		else
 		{
-			let [isingle, imultiple] = partition(inputs, (x) =>
-				some(single, ['origin', x.origin]))
-			return map(single, ({ path: fpath, origin }) =>
-			{
-				let o = reject(multiple, ['origin', origin])
-				let m = reject(implicit, ['origin', origin])
-				let i = find(isingle, ['origin', origin])
-				let d = reject(depends, ['origin', origin])
-				let a = reject(after, ['origin', origin])
-				return new Build(action, concat(o, { path: fpath }), m,
-					concat(imultiple, i), d, a, vars)
-			})
+			let [isingle, imultiple] = _.partition(inputs, (x) =>
+				_.some(single, ['origin', x.origin]))
+			return _.map(single,
+				/**
+				 * @param {ProductPath} target
+				 */
+				({ path: fpath, origin }) =>
+				{
+					let o = _.reject(multiple, ['origin', origin])
+					let m = _.reject(implicit, ['origin', origin])
+					let i = _.find(isingle, ['origin', origin])
+					let d = _.reject(depends, ['origin', origin])
+					let a = _.reject(after, ['origin', origin])
+					return new Build(action, _.concat(o, [{ path: fpath }]), m,
+						_.concat(imultiple, i), d, a, vars)
+				})
 		}
 	}
 }
@@ -346,7 +366,8 @@ module.exports = {
 	process_rules: process_rules,
 	process_variables: process_variables,
 	process_def_variables: process_def_variables,
-	process_pools: process_pools,
-	Writer: Writer,
-	Build: Build
+	process_pools: process_pools
 }
+
+module.exports.Writer = Writer
+module.exports.Build = Build
